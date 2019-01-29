14fc06596123a8f18ae8a2212ba64110dbfa1152
MC3D <m.chapman.home@gmail.com>
2019-01-29 10:13:22 -0500

    set up Django admin

--- tavern/admin.py
+++ tavern/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Location, Lunch
 
-# Register your models here.
+
+class LocationInline(admin.TabularInline):
+    model = Location
+    extra = 3
+
+
+class LunchAdmin(admin.ModelAdmin):
+    inlines = [LocationInline]
+
+
+admin.site.register(Location)
+admin.site.register(Lunch, LunchAdmin)
